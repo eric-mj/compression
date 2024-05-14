a53b9c7e523142f76faf9970e287deb42e8fb2b0
Eric Johnson <ericmjohnson1991@msn.com>
2024-05-14 13:24:59 -0700

    Formatting

--- src/rle.rs
+++ src/rle.rs
@@ -4,9 +4,9 @@ pub struct RunPair {
     run: usize,
 }
 
-pub fn rle_encode<T>(data: &[T]) -> Vec<RunPair> 
+pub fn rle_encode<T>(data: &[T]) -> Vec<RunPair>
 where
-    T: num::PrimInt + Ord + num::NumCast
+    T: num::PrimInt + Ord + num::NumCast,
 {
     let mut encoded_data = Vec::<RunPair>::new();
     let mut current_run_value = data.iter().next().unwrap();
@@ -15,13 +15,19 @@ where
         if val == current_run_value {
             current_run_len += 1;
         } else {
-            encoded_data.push(RunPair { value: current_run_value.to_usize().unwrap(), run: current_run_len });
+            encoded_data.push(RunPair {
+                value: current_run_value.to_usize().unwrap(),
+                run: current_run_len,
+            });
             current_run_value = val;
             current_run_len = 1;
         }
     }
-    encoded_data.push(RunPair { value: current_run_value.to_usize().unwrap(), run: current_run_len });
-    return  encoded_data;
+    encoded_data.push(RunPair {
+        value: current_run_value.to_usize().unwrap(),
+        run: current_run_len,
+    });
+    return encoded_data;
 }
 
 #[cfg(test)]
@@ -32,7 +38,14 @@ mod tests {
     fn test_rle() {
         let test_data = vec![1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 4, 5];
         let encoded_data = rle_encode(&test_data);
-        let truth = vec![RunPair{value: 1, run: 5}, RunPair{value: 2, run: 4}, RunPair{value: 3, run: 2}, RunPair{value: 4, run: 1}, RunPair{value: 5, run: 1}];
+        let truth = vec![
+            RunPair { value: 1, run: 5 },
+            RunPair { value: 2, run: 4 },
+            RunPair { value: 3, run: 2 },
+            RunPair { value: 4, run: 1 },
+            RunPair { value: 5, run: 1 },
+        ];
         assert_eq!(truth, encoded_data)
     }
 }
+
